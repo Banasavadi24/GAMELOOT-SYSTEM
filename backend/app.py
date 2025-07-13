3b6abff17ff783370c07e4f9a79c0a2f9a42a253
Bhumika Banasavadi Naganna <bhumikabn02@gmail.com>
2025-07-13 23:14:09 +0100

    change api address to work in local system

--- backend/app.py
+++ backend/app.py
@@ -5,7 +5,8 @@ import json
 import os
 
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "https://organic-zebra-pj9vj79jwvj9h66r-5500.app.github.dev"}})
+#CORS(app, resources={r"/*": {"origins": "https://organic-zebra-pj9vj79jwvj9h66r-5500.app.github.dev"}})
+CORS(app, resources={r"/*": {"origins": ["http://127.0.0.1:5500", "http://localhost:5500"]}})
 
 @app.route('/')
 def home():
